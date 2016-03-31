b7a06d836ced24f7c6a2218f0add2257b7999b96
Victor Algaze <valgaze@cisco.com>
2016-03-31 01:50:23 -0700

    Clean up for initial state

--- index.js
+++ index.js
@@ -2,27 +2,35 @@
 CLI TOOL SPECIFICATION:
 
 USAGE: $ node index.js [COMMAND] (parameters)
+NOTE: Paremeters should only be positive integers
 
 # Never reach the bottom area
 
-$ node index.js #returns "ERROR: No command entered"
-
 $ node index.js add 5 6 7 8 #returns 26
 
-$ node index.js xxyyyzzz 5 6 7 8#returns ”Sorry, we didn’t implement 'xxyyyzzz' yet!"
+$ node index.js #returns "ERROR: No command entered"
+
+$ node index.js xxyyyzzz #returns ”Sorry, we didn’t implement 'xxyyyzzz' yet!"
 
 $ node index.js help #returns "Please enter a command and parameters"
 
-$ node index.js add 5 6 7 somestring 8 #returns "ERROR: Sorry parameters must be numbers!"
+$ node index.js add 5 6 7 string 8 #returns "ERROR: Sorry parameters must be numbers!"
 
 $ node index.js add #returns "ERROR: No parameters provided"
 
-#If the result is less than 4 make the text cyan, 
-#If the result is less greater than 5 make it green,
-#If the sum is exactly 4 make it yellow background, blue text
+~~Color~~
+#If the result is less than 4 make the text green
+#If the result is exactly 4 make it yellow background with blue text
+#If the result is greater than 4, don't style it
 #If there's some type of error or problem give it red text
 
+For reference, you can use a compiled version of this CLI tool available globally as interviewDay to see the output
+
+$ interviewDay add 2 2
+
 */
+
+
 
 var fullInput;
 var params;
@@ -33,7 +41,7 @@ console.log('Here are the parameters', params);
 
 
 var commandLookupHash = {
-	"help":"Please enter a command and parameters",
+	"help":"[HELP] Please enter a command and parameters",
 	"add": function() {
 	var sum = 0;
 	for (var i = 0; i < arguments.lenght; i++){
@@ -99,3 +107,45 @@ console.log('######################################################');
 var breakyourapp = null;
 
 breakyourapp.push('oops.');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* this might be useful later-- feel free to use this or not
+
+
+var colorizer = function(input, textColor, backgroundColor) {
+	if (backgroundColor) {
+		return chalk[textColor][backgroundColor](input);
+	} else {
+		return chalk[textColor](input);
+	}
+};
+
+
+*/
